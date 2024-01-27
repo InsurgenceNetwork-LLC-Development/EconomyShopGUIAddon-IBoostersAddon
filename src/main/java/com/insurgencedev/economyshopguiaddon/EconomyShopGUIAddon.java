9bd496233faf756707c908826e438762e8122090
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-27 17:29:05 -0500

    api jar update

--- src/main/java/com/insurgencedev/economyshopguiaddon/EconomyShopGUIAddon.java
+++ src/main/java/com/insurgencedev/economyshopguiaddon/EconomyShopGUIAddon.java
@@ -1,16 +1,16 @@
 package com.insurgencedev.economyshopguiaddon;
 
 import com.insurgencedev.economyshopguiaddon.listeners.EconomyShopEventListener;
-import org.bukkit.Bukkit;
 import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
 import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
+import org.insurgencedev.insurgenceboosters.libs.fo.Common;
 
 @IBoostersAddon(name = "EconomyShopGUIAddon", version = "1.0.1", author = "InsurgenceDev", description = "EconomyShopGUI Support")
 public class EconomyShopGUIAddon extends InsurgenceBoostersAddon {
 
     @Override
-    public void onAddonReloadablesStart() {
-        if (Bukkit.getPluginManager().isPluginEnabled("EconomyShopGUI")) {
+    public void onAddonReloadAblesStart() {
+        if (Common.doesPluginExist("EconomyShopGUI")) {
             registerEvent(new EconomyShopEventListener());
         }
     }
--- src/main/java/com/insurgencedev/economyshopguiaddon/listeners/EconomyShopEventListener.java
+++ src/main/java/com/insurgencedev/economyshopguiaddon/listeners/EconomyShopEventListener.java
@@ -4,28 +4,27 @@ import me.gypopo.economyshopgui.api.events.PreTransactionEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.insurgencedev.insurgenceboosters.api.IBoosterAPI;
-import org.insurgencedev.insurgenceboosters.models.booster.GlobalBoosterManager;
-import org.insurgencedev.insurgenceboosters.settings.IBoostersPlayerCache;
+import org.insurgencedev.insurgenceboosters.data.BoosterFindResult;
 
 public final class EconomyShopEventListener implements Listener {
 
     @EventHandler
     public void onSell(PreTransactionEvent event) {
-        String type = "Sell";
+        final String TYPE = "Sell";
         final String NAMESPACE = "ECONOMY_SHOPGUI";
-        double totalMulti = 1;
+        final double[] totalMulti = {1};
 
-        IBoostersPlayerCache.BoosterFindResult pResult = IBoosterAPI.getCache(event.getPlayer()).findActiveBooster(type, NAMESPACE);
-        if (pResult instanceof IBoostersPlayerCache.BoosterFindResult.Success boosterResult) {
-            totalMulti += boosterResult.getBooster().getMultiplier();
+        BoosterFindResult pResult = IBoosterAPI.INSTANCE.getCache(event.getPlayer()).getBoosterDataManager().findActiveBooster(TYPE, NAMESPACE);
+        if (pResult instanceof BoosterFindResult.Success boosterResult) {
+            totalMulti[0] += boosterResult.getBoosterData().getMultiplier();
         }
 
-        GlobalBoosterManager.BoosterFindResult gResult = IBoosterAPI.getGlobalBoosterManager().findBooster(type, NAMESPACE);
-        if (gResult instanceof GlobalBoosterManager.BoosterFindResult.Success boosterResult) {
-            totalMulti += boosterResult.getBooster().getMultiplier();
-        }
+        IBoosterAPI.INSTANCE.getGlobalBoosterManager().findGlobalBooster(TYPE, NAMESPACE, globalBooster -> {
+            totalMulti[0] += globalBooster.getMultiplier();
+            return null;
+        }, () -> null);
 
-        event.setPrice(calculateAmount(event.getAmount(), totalMulti));
+        event.setPrice(calculateAmount(event.getAmount(), totalMulti[0]));
     }
 
     private long calculateAmount(double amount, double multi) {
